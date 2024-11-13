f144bd4b74e231a134996cb098739fb4d3f1d30d
maiducbinh <binhmai2205@gmail.com>
2024-11-13 13:58:24 +0700

    fixing order

--- src/main/java/com/ecom/controller/ChatController.java
+++ src/main/java/com/ecom/controller/ChatController.java
@@ -1,20 +1,33 @@
 package com.ecom.controller;
 
+import com.ecom.model.UserDtls;
+import com.ecom.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
 // Thay đổi từ @RestController sang @Controller
 @Controller
 public class ChatController {
+    @Autowired
+    private UserService userService;
+
+    private UserDtls getLoggedInUserDetails(Principal p) {
+        String email = p.getName();
+        UserDtls userDtls = userService.getUserByEmail(email);
+        return userDtls;
+    }
 
     @GetMapping("/user/chat")
-    public String showChatPage() {
+    public String showChatPage(Principal p) {
+        UserDtls user = getLoggedInUserDetails(p);
         return "chat";
     }
 }
